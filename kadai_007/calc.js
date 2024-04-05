fef8d4d1c616fbc9a3a6bea39e12cb709aa7dd98
plusquehabi <165114710+plusquehabi@users.noreply.github.com>
2024-04-05 16:21:34 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,63 @@
+let num = 15;
+
+if (num % 5 === 0 && num % 3 === 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 === 0) {
+    console.log('3の倍数です');
+} else if (num % 5 === 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+ 
+num = 10;
+
+if (num % 5 === 0 && num % 3 === 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 === 0) {
+    console.log('3の倍数です');
+} else if (num % 5 === 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+
+num = 6;
+
+if (num % 5 === 0 && num % 3 === 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 === 0) {
+    console.log('3の倍数です');
+} else if (num % 5 === 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+
+num = 7;
+
+if (num % 5 === 0 && num % 3 === 0) {
+    console.log('3と5の倍数です');
+} else if (num % 3 === 0) {
+    console.log('3の倍数です');
+} else if (num % 5 === 0) {
+    console.log('5の倍数です');
+} else {
+    console.log(num);
+}
+
+
+// 100までの数の場合
+for (let num = 1; num <= 100; num++) {
+    
+    if (num % 5 === 0 && num % 3 === 0) {
+        console.log('3と5の倍数です');
+    } else if (num % 3 === 0) {
+        console.log('3の倍数です');
+    } else if (num % 5 === 0) {
+        console.log('5の倍数です');
+    } else {
+        console.log(num);
+    }
+
+}
